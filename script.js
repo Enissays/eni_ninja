758816a0997590e5c7bd89c748862012fefd17a0
Enissays <yassinebendali@outlook.fr>
2022-05-16 22:59:28 +0100

    Séparation du clicker dans un autre onglet, ajout de div boutons (a optimiser), ajout de nouvelles polices et changement de fond

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function anyaHehe()
     var el = document.getElementById("counter");
     el.textContent = parseInt(el.textContent) + 1;
 }
+
+function blueIfy(id, on) 
+{
+    var el = document.getElementById(id);
+    if (on) el.style.backgroundColor = '#090979';
+    else el.style.backgroundColor = '#4287f5';
+}
